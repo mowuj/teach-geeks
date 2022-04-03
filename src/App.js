62f13cc93717a9c5f092c8ba8533122d8039f661
mowuj <mowuj92@gmail.com>
2022-04-03 15:24:34 +0600

    navbar set  Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Navbar from './Components/Navbar/Navbar';
+import NotFound from './Components/NotFound/NotFound';
+import Videos from './Components/Videos/Videos';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <>
+      <Navbar></Navbar>
+    <Routes>(
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/videos' element={<Videos></Videos>}></Route>
+      <Route path='/login' element={<Login></Login>}></Route>
+      <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      </>
+    );
 }
 
 export default App;
---
+++ src/Components/Videos/Videos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Videos = () => {
+    return (
+        <div>
+            <h2>this is videos page</h2>
+        </div>
+    );
+};
+
+export default Videos;
